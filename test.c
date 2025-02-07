02359e184806937125c9c241808208534faabdfb
Murakami Aota <e225737@ie.u-ryukyu.ac.jp>
2025-02-07 16:56:24 +0900

    test

---
+++ test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    for (int i = 0; i < 10; i++) { // 初期化: int i = 1, 条件: i <= 10, 更新: i++
+        printf("%d\n", i);         // i の値を出力
+    }
+    return 0;
+}
